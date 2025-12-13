2481472756023065e6522ab5a61a6f418ed72e0d
MISCUAMATUS <kumdofaku2@gmail.com>
2025-12-13 17:37:14 -0300

    Add files via upload
    agrega un ejercicio nuevo de la prueba 3 sobre fetch

--- main.js
+++ main.js
@@ -144,8 +144,6 @@ function ejercicioZ(event){
     //NOTA: El parrafo puede contener el contendido del vector en su texto sin usar JSON.stringify, asi que no sabria decir que utilidad tiene esto.
 }
 
-
-
 let frutas = ['manzana', 'banana', 'cereza'];
 
 // Añadir al final
@@ -167,5 +165,57 @@ frutas.splice(1, 0, 'uva'); // ['manzana', 'uva', 'banana', 'cereza']
 let textoFrutas = frutas.join(' y '); // "manzana y uva y banana y cereza"
 
 console.log(frutas);
+
+function ejercicioZ2(event){  //El usuario modifica las propiedades CSS de un elemento.
+
+    event.preventDefault();
+
+    propiedades = event.target;
+    altura  = propiedades.height+"px";
+    anchura = propiedades.width+"px";
+    relleno = propiedades.padding+"px";
+    margen  = propiedades.margin+"px";
+
+    elemento = document.querySelector('#CambiaForma');
+
+    elemento.style.width   = anchura;
+    elemento.style.height  = altura;
+    elemento.style.padding = relleno;
+    elemento.style.margin  = margen;
+
+    elemento.textContent = "Hay que arregar el ejercicio";
+    verde = ramdomHasta255();
+    azul = ramdomHasta255();
+    rojo = ramdomHasta255();
+   elemento.style.backgroundColor = `rgb(${verde}, ${azul}, ${rojo})`
+}
+
+//PRUEBA 3
+
+let url = null;
+
+function ejercicio325(event){
+
+    url = 'https://fakestoreapi.com/products/'         //Cada vez que escribamos url estamos poniendo el link de la api/ a la api.
+    
+    event.preventDefault();                                 //Evitar que NUESTRA pagina reinicie.
+    id = event.target.id.value;                             //Obtenemos el valor del campo o input id del formulario.
+    console.log(url+id);                                    //Pone el link del producto en la consola.
+    
+        fetch(url + id)                                     //Hace una peticion al link. 
+        .then(res => res.json())                            //Recibe la respuesta.
+        .then(data => {                                     //Los datos:
+            productos = data;                               // 1 - Los guardamos en la variable productos.
+            console.log(productos);                         // 2 - Los mostramos en consola, usando la variable productos.
+           
+            alert("La información del producto con el id " + id + " fue recibida.");
+            alert("Hace click derecho en la página, apreta inspeccionar y después consola.");}
+            )
+
+        .catch(error => {                                   //Si hay un error pasa esto.
+      console.error('Error al obtener el producto:', error);
+      alert("Hubo un error, fijate en la consola el codigo.");}
+    );
+}
 
 
